85b3cc228309fa0d4c2e5830df7e3328a4c00372
ignaciosm <ignaciosm@gmail.com>
2019-11-06 20:00:09 -0500

    mvp

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { connect } from "react-redux";
+import { removeFeature } from '../actions';
 
 const AddedFeature = props => {
+
+  const handleRemoveFeature = () => {
+    props.removeFeature(props.feature)
+    // console.log('from handleRemoveFeature', props.feature)
+  }
+
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button onClick={handleRemoveFeature} className="button">X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapDispatchToProps = {
+  removeFeature
+};
+
+export default connect(
+  state => state,
+  mapDispatchToProps
+)(AddedFeature);
